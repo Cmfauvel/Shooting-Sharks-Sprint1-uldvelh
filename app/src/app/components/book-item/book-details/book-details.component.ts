06c8ede825aa198e4302bd32eab654ac0e7fc768
Cmfauvel <Cm.fauvel@gmail.com>
2021-06-04 22:15:20 +0200

    comments book-details avant fix

--- app/src/app/components/book-item/book-details/book-details.component.ts
+++ app/src/app/components/book-item/book-details/book-details.component.ts
@@ -14,11 +14,11 @@ chapters;
     private chapterService: ChapterService) { }
 
   ngOnInit(): void {
-    const idChapter = this.activatedRoute.snapshot.params['id'];
-    const chapters = this.chapterService.chapters.slice();
-    this.chapter = chapters.find(chapter => chapter.id == idChapter);
-    console.log(chapters[1].text, idChapter)
-    this.chapters = chapters;
+    // const idChapter = this.activatedRoute.snapshot.params['id'];
+    // const chapters = this.chapterService.chapters.slice();
+    // this.chapter = chapters.find(chapter => chapter.id == idChapter);
+    // console.log(chapters[1].text, chapters[1].idChapter)
+    // this.chapters = chapters;
   }
 
 }
