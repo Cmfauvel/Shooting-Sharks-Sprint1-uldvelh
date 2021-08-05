3bbee150854a117f9b4a227d4291fb63e1a18191
Cmfauvel <Cm.fauvel@gmail.com>
2021-08-05 10:21:40 +0200

    merge release/caro

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,12 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { LibraryComponent } from './components/library/library.component';
 import { LibraryOfOneCreatorComponent } from './components/library-of-one-creator/library-of-one-creator.component';
 import { BookItemComponent } from './components/book-item/book-item.component';
-import { FooterComponent } from './footer/footer.component';
 import { BookDetailsComponent } from './components/book-item/book-details/book-details.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
@@ -23,8 +21,10 @@ import { BookService } from './services/book.service';
 import { CreatorService } from './services/creator.service';
 import { HeroMakerComponent } from './components/hero-maker/hero-maker.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
-import { RegisterComponent } from './components/register/register.component';
 import { ParamBookComponent } from './components/param-book/param-book.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
